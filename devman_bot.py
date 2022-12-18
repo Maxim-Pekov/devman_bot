370ce89dc4ee17f57ff07217c719b727dcf3ff38
Maxim-Pekov <max.pekov@gmail.com>
2022-12-18 19:33:29 +0300

    Add function to send messege if my homework check

--- devman_bot.py
+++ devman_bot.py
@@ -16,15 +16,13 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def start(update, context):
-    context.bot.send_message(
-        chat_id=update.message.chat_id,
-        text=f'HiHI {update.message.chat.username}')
+# def start(update, context):
+#     context.bot.send_message(
+#         chat_id=update.message.chat_id,
+#         text=f'HiHI {update.message.chat.username}')
 
 
-if TOKEN_TG == "":
-    print("Please write TOKEN into file")
-else:
+def reply_message(title):
     request = Request(
         connect_timeout=0.5,
         read_timeout=1.0,
@@ -33,12 +31,29 @@ else:
         request=request,
         token=TOKEN_TG,
     )
-    updater = Updater(TOKEN_TG, use_context=True)
-    dispatcher = updater.dispatcher
+    bot.send_message(
+        chat_id=741289598,
+        text=f'ваша работа "{title}" проверена!!'
+    )
+# reply_message()
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    # dispatcher.add_handler(CommandHandler("calendar", calendar_handler))
-    # dispatcher.add_handler(CallbackQueryHandler(inline_handler))
-
-    updater.start_polling()
-    updater.idle()
+# if TOKEN_TG == "":
+#     print("Please write TOKEN into file")
+# else:
+#     request = Request(
+#         connect_timeout=0.5,
+#         read_timeout=1.0,
+#     )
+#     bot = Bot(
+#         request=request,
+#         token=TOKEN_TG,
+#     )
+#     updater = Updater(TOKEN_TG, use_context=True)
+#     dispatcher = updater.dispatcher
+#
+#     dispatcher.add_handler(CommandHandler("start", start))
+#     # dispatcher.add_handler(CommandHandler("calendar", calendar_handler))
+#     # dispatcher.add_handler(CallbackQueryHandler(inline_handler))
+#
+#     updater.start_polling()
+#     updater.idle()
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests, os
-
+from devman_bot import reply_message
 from pprint import pprint
 from dotenv import load_dotenv
 from time import sleep
+from telegram import Bot
 
 
 load_dotenv()
 DEVMAN_TOKEN = os.getenv('DEVMAN_TOKEN')
-TG_BOT = os.getenv('TG_BOT')
+TOKEN_TG = os.getenv('TOKEN_TG')
 TIMEOUT = 10
 
 
@@ -17,17 +18,15 @@ def main():
             params = {
                 'timestamp': '',
             }
-            url = 'https://dvmn.org/api/user_reviews/'
             long_polling = 'https://dvmn.org/api/long_polling/'
-            # response = requests.get(url, headers={
-            #     'Authorization': f'Token {DEVMAN_TOKEN}'
-            # })
             response = requests.get(long_polling, headers={
                 'Authorization': f'Token {DEVMAN_TOKEN}'
             }, timeout=TIMEOUT, params=params)
             params['timestamp'] = response.json().get('new_attempts')[0].get('timestamp')
+            lesson_title = response.json().get('new_attempts')[0].get('lesson_title')
             pprint(params['timestamp'])
-            # pprint(response.text)
+            pprint(response.json())
+            reply_message(lesson_title)
         except requests.exceptions.ReadTimeout:
             print(111)
             pass
