8d9accf8fb91b21c2c594c3f706107a5352b8183
Maxim-Pekov <max.pekov@gmail.com>
2022-12-18 18:29:59 +0300

    add timestamp to requests params

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from time import sleep
 
 load_dotenv()
 DEVMAN_TOKEN = os.getenv('DEVMAN_TOKEN')
+TIMEOUT = 10
 
 
 def main():
     while True:
         try:
+            params = {
+                'timestamp': '',
+            }
             url = 'https://dvmn.org/api/user_reviews/'
             long_polling = 'https://dvmn.org/api/long_polling/'
             # response = requests.get(url, headers={
@@ -19,15 +23,16 @@ def main():
             # })
             response = requests.get(long_polling, headers={
                 'Authorization': f'Token {DEVMAN_TOKEN}'
-            }, timeout=10)
-
-            pprint(response.text)
+            }, timeout=TIMEOUT, params=params)
+            params['timestamp'] = response.json().get('new_attempts')[0].get('timestamp')
+            pprint(params['timestamp'])
+            # pprint(response.text)
         except requests.exceptions.ReadTimeout:
             print(111)
             pass
         except requests.exceptions.ConnectionError:
             print(222)
-            sleep(10)
+            sleep(TIMEOUT)
             pass
 
 
