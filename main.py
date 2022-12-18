90374f5dba97bb1fc86032e4950946e517a7ecc1
Maxim-Pekov <max.pekov@gmail.com>
2022-12-18 17:23:39 +0300

    write try/except ReadTimeout, ConnectionError

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
+import requests, os
+
 from pprint import pprint
+from dotenv import load_dotenv
+from time import sleep
 
-import requests
+
+load_dotenv()
+DEVMAN_TOKEN = os.getenv('DEVMAN_TOKEN')
 
 
-def print_hi(name):
-    token = {'Authorization': 'Token 33811728b3925fac31e3f92d7b9fc820679551a8'}
-    url = 'https://dvmn.org/api/user_reviews/'
-    long_polling = 'https://dvmn.org/api/long_polling/'
-    # response = requests.get(url, headers=token)
-    response = requests.get(long_polling, headers=token)
+def main():
+    while True:
+        try:
+            url = 'https://dvmn.org/api/user_reviews/'
+            long_polling = 'https://dvmn.org/api/long_polling/'
+            # response = requests.get(url, headers={
+            #     'Authorization': f'Token {DEVMAN_TOKEN}'
+            # })
+            response = requests.get(long_polling, headers={
+                'Authorization': f'Token {DEVMAN_TOKEN}'
+            }, timeout=10)
 
-    pprint(response.text)
+            pprint(response.text)
+        except requests.exceptions.ReadTimeout:
+            print(111)
+            pass
+        except requests.exceptions.ConnectionError:
+            print(222)
+            sleep(10)
+            pass
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
+
 
 
